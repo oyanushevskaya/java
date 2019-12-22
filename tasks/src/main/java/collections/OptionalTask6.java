a70eed24a0367b76597831ac2d05733c3fef5cd0
Volha Yanusheuskaya <olga.yanushevskaya0@gmail.com>
2019-12-22 22:05:44 +0300

    Fix some bugs

--- tasks/src/main/java/collections/OptionalTask6.java
+++ tasks/src/main/java/collections/OptionalTask6.java
@@ -7,11 +7,11 @@ import java.util.*;
 //6. Ввести строки из файла, записать в список ArrayList. Выполнить сортировку строк, используя метод sort() из класса Collections.
 public class OptionalTask6 {
     public static void main(String[] args) throws IOException {
-        final String PACKAGE = "src/main/java/";
+        final String PACKAGE = "data/";
         final String EXT = ".txt";
         try (FileReader fileReader = new FileReader(PACKAGE + "in" + EXT)) {
             Scanner sc = new Scanner(fileReader);
-            List<String> list = new ArrayList<String>();
+            List<String> list = new ArrayList<>();
             while (sc.hasNext()) {
                 String lineNext = sc.nextLine();
                 list.add(lineNext);
