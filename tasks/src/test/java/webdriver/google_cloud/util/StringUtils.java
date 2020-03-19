f7e507c8210a1857664d567f00d17b1db5f921d6
Volha Yanusheuskaya <olga.yanushevskaya0@gmail.com>
2020-03-19 18:40:45 +0300

    Fix review comments for Module 2.5

---
+++ tasks/src/test/java/webdriver/google_cloud/util/StringUtils.java
@@ -0,0 +1,27 @@
+package webdriver.google_cloud.util;
+
+public class StringUtils {
+  public final static String HOMEPAGE_URL = "https://cloud.google.com/";
+  public final static String EMAIL_PAGE_URL = "https://10minutemail.com/";
+
+  public final static String TERM_FOR_SEARCH = "Google Cloud Platform Pricing Calculator";
+
+  public final static String TOTAL_ESTIMATED_COST = "Total Estimated Cost: USD 1,082.77 per 1 month";
+  public final static String ESTIMATED_MONTHLY_COST = "Estimated Monthly Cost: USD 1,082.77";
+  public final static String ERROR_TOTAL_COST = "Total Estimated Cost is not right!";
+
+  public final static String MACHINE_CLASS_REGULAR = "Regular";
+  public final static String ERROR_MACHINE_CLASS = "Machine class is not " + MACHINE_CLASS_REGULAR;
+
+  public final static String MACHINE_TYPE_N1STANDART8 = "n1-standard-8 (vCPUs: 8, RAM: 30GB)";
+  public final static String ERROR_MACHINE_TYPE = "Machine type is not " + MACHINE_TYPE_N1STANDART8;
+
+  public final static String REGION_FRANKFURT= "Frankfurt (europe-west3)";
+  public final static String ERROR_REGION = "Region is not " + REGION_FRANKFURT;
+
+  public final static String LOCAL_SSD_2x375GB= "2x375 GB";
+  public final static String ERROR_LOCAL_SSD = "Local SSD is not " + LOCAL_SSD_2x375GB;
+
+  public final static String USAGE_1YEAR = "1 Year";
+  public final static String ERROR_USAGE = "Committed usage is not " + USAGE_1YEAR;
+}
